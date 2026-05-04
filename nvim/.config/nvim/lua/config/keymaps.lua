ecb6770da0761a462ef62628711d7b85e7ac1d79
Ryan Good <ryan.good@zetier.com>
2026-05-03 21:37:22 -0500

    switch to lazy

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,24 +1,3 @@
-local map = vim.keymap.set
-
--- Clear search highlights
-map("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- Better window navigation
-map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
-
--- Resize with arrows
-map("n", "<C-Up>", "<cmd>resize +2<CR>")
-map("n", "<C-Down>", "<cmd>resize -2<CR>")
-map("n", "<C-Left>", "<cmd>vertical resize -2<CR>")
-map("n", "<C-Right>", "<cmd>vertical resize +2<CR>")
-
--- Move lines up/down in visual mode
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
-
--- Keep cursor centered when scrolling
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
--- nvim/.config/nvim/setup.sh
+++
@@ -1,3 +0,0 @@
-curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip && unzip FiraCode.zip
-mv FiraCode.zip .local/share/fonts/
-fc-cache -fv
