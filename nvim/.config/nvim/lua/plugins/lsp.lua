b62a833cc885f818ef16596681a0540165ab329e
Ryan Good <ryan.good@zetier.com>
2026-05-04 19:50:09 -0500

    fix e501

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,20 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              flake8 = {
+                ignore = { "E501" },
+              },
+              pycodestyle = {
+                ignore = { "E501" },
+              },
+            },
+          },
+        },
+      },
       pyright = {
         settings = {
           pyright = {
